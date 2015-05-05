2d397eb32d415d9499b460dd80ae72457740796e
Thomas Levine <_@thomaslevine.com>
2015-05-05 10:33:24 +0000

    change setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'nyc-crime-map',
-    version = '0.1',
+    version = '0.2',
     description = 'Get data from the crime map',
     author = 'Thomas Levine',
     author_email = '_@thomaslevine.com',
-    url = 'http://thomaslevine.com/!/nyc-crime-map',
+    url = 'http://thomaslevine.com/!/nyc-crime-map/',
+    entry_points = {'console_scripts': ['nyc-crime-map = nyc_crime_map:cli']},
     license = 'AGPL',
-    scripts = ['nyc-crime-map'],
+    packages = ['nyc_crime_map'],
     install_requires = [
         'requests>=2.3.0',
+        'vlermv>=1.0.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
     ],
 )
