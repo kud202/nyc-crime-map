ee7e08184b3c16954d135d44dbb791fe3219c729
Thomas Levine <_@thomaslevine.com>
2015-05-07 23:00:40 +0000

    -

--- nyc_crime_map/__init__.py
+++ nyc_crime_map/__init__.py
@@ -1,9 +1,24 @@
+def nyc_crime_map(table_id, select, startPageToken = None):
+    if startPageToken:
+        pageToken = startPageToken
+    else:
+        logger.info('Loading data for the initial search, without pageToken')
+        results = table_features_first(table_id, select).json()
+        yield from results.get('features', [])
+        pageToken = results.get('nextPageToken')
 
-def main():
+    while pageToken:
+        logger.info('Loading data for pageToken %s' % pageToken)
+        results = table_features_tail(table_id, select, pageToken).json()
+        yield from results.get('features', [])
+        pageToken = results.get('nextPageToken')
+
+def cli():
     for table_id, select in [
          ('02378420399528461352-17772055697785505571', 'YR,MO,geometry,X,Y,TOT,CR'),
          ('02378420399528461352-11853667273131550346', 'YR,MO,geometry,X,Y,TOT'),
     ]:
+        data = nyc_crime_map(table_id, select)
         head(table_id, select)
         to_csv(table_id, select)
         to_geojson(table_id, select)
--- nyc_crime_map/download.py
+++ nyc_crime_map/download.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
-import csv
-import os
-import json
 import logging
-
-from string import ascii_uppercase
-import itertools
 
 import vlermv
 import requests
 
-logger = logging.getLogger
+logger = logging.getLogger(__name__)
 
 KEY = 'AIzaSyDW3Wvk6xWLlLI6Bfu29DuDaseX-g18_mo'
 
-@vlermv.cache('~/.vlermv', key_transformer = vlermv.transformers.archive())
-def table_features(table_id, select, where = None, maxResults = 1000, pageToken = None):
+@vlermv.cache('~/.nyc-crime-map/page',
+    key_transformer = vlermv.transformers.archive())
+def page(table_id, select, pageToken = None):
+    '''
+    Args: A pageToken or None
+    Returns: The next pageToken or None
+    '''
+
+    r = table_features(table_id, select, maxResults = 1000)
+
+
+@vlermv.cache('~/.nyc-crime-map/table_features_tail',
+    key_transformer = vlermv.transformers.archive())
+def table_features_startpage(table_id, select, pageToken):
+    return _table_features(table_id, select, pageToken)
+
+@vlermv.cache('~/.nyc-crime-map/table_features_first',
+    key_transformer = vlermv.transformers.archive())
+def table_features_startpage(table_id, select):
+    return _table_features(table_id, select, None)
+
+def _table_features(table_id, select, pageToken, where = None, maxResults = 1000):
     url = 'https://www.googleapis.com/mapsengine/v1/tables/%s/features/' % table_id
 
     params = {
@@ -34,20 +48,3 @@ def table_features(table_id, select, where = None, maxResults = 1000, pageToken 
         'Referer':  'http://maps.nyc.gov/crime/',
     }
     return requests.get(url, headers = headers, params = params)
-
-def features(table_id, select, startPageToken = None):
-    if startPageToken:
-        pageToken = startPageToken
-    else:
-        logger.info('Loading data for the initial search, without pageToken')
-        results = page(table_id, select)
-        for result in results.get('features', []):
-            yield result
-        pageToken = results.get('nextPageToken')
-
-    while pageToken:
-        logger.info('Loading data for pageToken', pageToken)
-        results = page(table_id, select, pageToken = pageToken)
-        for result in results.get('features', []):
-            yield result
-        pageToken = results.get('nextPageToken')
